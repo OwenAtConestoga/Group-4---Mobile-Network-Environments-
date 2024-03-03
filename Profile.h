668d8beb9af08f2ced3d871140b3b45e307c525b
Louis He <louis520321@gmail.com>
2024-03-02 23:02:28 -0500

    Update the sturcture of profile and some function

---
+++ Profile.h
@@ -0,0 +1,50 @@
+#pragma once
+#ifndef PROFILE_H
+#define PROFILE_H
+#include <string>
+#include <fstream>
+#include <iostream>
+
+struct Profile 
+{
+    //Basic Account setting
+    std::string userName;
+    std::string firstName;
+    std::string lastName;
+    std::string bio;
+    std::string userClass;
+    int id;
+    int voteCount;
+
+    //Set up basic initialize the profile with provided data
+    Profile(const std::string& uName, const std::string& fName, const std::string& lName,
+        const std::string& b, const std::string& uClass, int i, int vCount)
+        : userName(uName), firstName(fName), lastName(lName),
+        bio(b), userClass(uClass), id(i), voteCount(vCount) {}
+
+    // swich the profile struct converts the profile's attributes into a single string, if no need now, we can delate it
+    std::string serialize() const 
+    {
+        return userName + "," + firstName + "," + lastName + "," +
+            bio + "," + userClass + "," + std::to_string(id) + "," +
+            std::to_string(voteCount);
+    }
+
+    //Save the profile data into file
+    void saveToFile(const std::string& filePath) const 
+    {
+        std::ofstream outFile(filePath, std::ios::app);
+        if (!outFile) 
+        {
+            std::cerr << "Could not open file for writing: " << filePath << std::endl;
+            return;
+        }
+        outFile << serialize() << std::endl;
+        outFile.close();
+    }
+
+   
+};
+
+#endif // PROFILE_H
+
