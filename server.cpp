d4a1a32cc1a8782451d3ecc3de1df477a861d1b9
Yuval Rashish <yrashish3335@conestogac.on.ca>
2024-03-14 01:44:16 -0400

    Seperated profile header from packet header

--- server.cpp
+++ server.cpp
@@ -10,20 +10,25 @@ int main()
 	ServerConnection server;
 	WSAData wsaData;
 	int bytesReceived;
-
+	int packetType;
+	// Sets up the server
 	if (WSAStartup(MAKEWORD(2, 2), &wsaData) != 0) { return 0; }
 	if (server.setServerSocket(socket(AF_INET, SOCK_STREAM, IPPROTO_TCP)) != 0) { return 0; }
 	if (server.socketCreateBind()) { return 0; }
 	if (server.socketListenConnect()) { return 0; }
 	if (server.setConnectionSocket() != 0){	return 0; }
 
+	// Accepts and recieves messages
 	do 
 	{
 		bytesReceived = server.recvMsg();
 		server.changeRxBuffer(server.getRxBuffer() + '\0');
-		// Deserialize the received data into a Profile object and save/update
-		Profile profile = Profile::deserialize(string(server.getRxBuffer()));
-		profile.saveToFile("profiles.txt"); // Save to prifile.txt, if need we can change the path
+
+		switch (packetType) {
+		case(0): //profile packet
+			Profile profile(server.getRxBuffer()); //creates a profile from the packet
+		}
+		
 	} while (bytesReceived > 0);
 
 	server.closeServer();
